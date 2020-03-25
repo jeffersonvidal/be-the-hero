212f2f751665ae62d32f40a86c4a8044ed4b37da
jeffersonvidal <vidalmoc@gmail.com>
2020-03-24 22:34:13 -0300

    Instalado o CORS para dar permissões de acesso a API backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,9 @@
 const express = require('express'); //importando funcionalidades do express
+const cors = require('cors'); //importando cors - dá permissão de acesso a api
 const routes = require('./routes'); //importando arquivo de rotas
 
 const app = express(); //instanciando express na variável app
+app.use(cors()); //qualquer aplicação pode acessar o backend
 app.use(express.json());// aplicação vai trabalhar com json
 app.use(routes);
 
